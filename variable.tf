14fc93d565937052e21d5404ed0847f8617c4ce0
AkshayDevOps <134368829+Akshayprac895@users.noreply.github.com>
2025-07-11 15:58:22 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,22 @@
+variable "aws_region" {
+   default =  "us-east-1"
+}
+
+
 variable "instance_type" {
    description = "Instance type" 
    default = "t3.medium"
 }
 
+variable "ami_id"  {
+   default = "ami-05ffe3c48a9991133"
+}
 
-  
+variable "key_name" {
+  description = "SSH key pair name"
+  default     = "my-key-pair"
+}
+
+variable "instance_name" {
+  default = "MyEC2Instance"
+}
