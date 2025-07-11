9184cd6e54131d3a4f3591ff0d702b703b1507c2
AkshayDevOps <134368829+Akshayprac895@users.noreply.github.com>
2025-07-11 15:53:43 +0530

    Create main.tf for ec2

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+
+resource "aws_instance" "ec2_instance" {
+  ami =  var.ami_id
+  instance_type = var.instance_type
+  key_name = var.key_name
+
+
+  tag {
+    name = var.instance_name
+  }
+}
