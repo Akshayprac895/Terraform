43559a1482a5c0e728bed2c0a51fb4823cf74519
Akshay Shivanand Arankar <akshay.arankar@yash.com>
2025-07-23 15:38:03 +0530

    update provider [skip ci]

--- provider.tf
+++ provider.tf
@@ -1,3 +1,27 @@
 provider "aws" {
   region =  "us-east-1"
 }
+
+# if you want to deploy the ec2 or any resources in multi region then you have setup sturcture like this 
+
+# provider "aws" {
+#   alias = "us-east-1"
+#   region =  "us-east-1"
+# }
+
+# provider "aws" {
+#   alias = "us-east-2"
+#   region =  "us-east-2"
+# }
+
+# resource "aws_instance" "test" {
+#   ami = "ami-05ffe3c48a9991133"
+#   instance_type = "t3.medium"
+#   provider = "aws.us-east-1"
+# }
+
+# resource "aws_instance" "test1" {
+#   ami = "ami-05ffe3c48a9991133"
+#   instance_type = "t3.medium"
+#   provider = "aws.us-east-2"
+# }
