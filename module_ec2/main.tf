45a6fcea73ee1a87584b49ab85723eee48ef402d
AkshayDevOps <134368829+Akshayprac895@users.noreply.github.com>
2025-07-25 12:51:51 +0530

    Create main.tf [skip ci]

---
+++ module_ec2/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "ec2_instance" "example" {
+    ami =  "var.ami_id"
+    instance_type = "var.instance_type"
+    subnet_id = "var.subnet_id"
+}
+#value 
+variable "ami_id" {
+    description = "Id of ami"
+}
+
+variable "instance_type" {
+    description = -"instance type"
+}
