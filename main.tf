42ea62e328b53bd8a22d92d815a3dd3f9b4091ec
AkshayDevOps <134368829+Akshayprac895@users.noreply.github.com>
2025-07-25 13:09:09 +0530

    Update main.tf [skip ci]

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
 
+module "ec2_instance" {
+  source        = "./terraform/module_ec2"  # path to your module_ec2
+  ami_id        = "ami-05ffe3c48a9991133"
+  instance_type = "t3.medium"
+  subnet_id = "subnet-02acf2c93130b4eef"
+}
 
-resource "aws_instance" "main" {
-  ami =  var.ami_id
-  instance_type = var.instance_type
+
+
+
+#resource "aws_instance" "main" {
+#  ami =  var.ami_id
+#  instance_type = var.instance_type
+#}
 
 
 
