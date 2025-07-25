dad3e3b0994f35b840aee683cfbcdd34666afd3e
AkshayDevOps <134368829+Akshayprac895@users.noreply.github.com>
2025-07-25 15:10:38 +0530

    Create variable.tf [skip ci]

---
+++ module_ec2/variable.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+   default =  "us-east-1"
+}
+
+
+variable "instance_type" {
+   description = "Instance type" 
+   default = "t3.medium"
+}
+
+variable "ami_id"  {
+   default = "ami-05ffe3c48a9991133"
+}
+
+variable "subnet_id" {
+    default = "subnet-02acf2c93130b4eef"
+}
