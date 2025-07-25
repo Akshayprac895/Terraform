ae3737d7453a0106b298ab83248f2ed46c36a6fe
AkshayDevOps <134368829+Akshayprac895@users.noreply.github.com>
2025-07-25 15:42:03 +0530

    Update main.tf [skip ci]

--- module_ec2/main.tf
+++ module_ec2/main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+   region = "us-east-1"
+}
 
 
 resource "ec2_instance" "example" {
@@ -5,11 +8,4 @@ resource "ec2_instance" "example" {
     instance_type = "var.instance_type"
     subnet_id = "var.subnet_id"
 }
-#value 
-variable "ami_id" {
-    description = "Id of ami"
-}
 
-variable "instance_type" {
-    description = "instance type"
-}
