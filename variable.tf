0d5c3ead8b4f7229b164bf684525c56bbf7eac9f
AkshayDevOps <134368829+Akshayprac895@users.noreply.github.com>
2025-07-25 15:36:21 +0530

    Update variable.tf [skip ci]

--- variable.tf
+++ variable.tf
@@ -12,6 +12,10 @@ variable "ami_id"  {
    default = "ami-05ffe3c48a9991133"
 }
 
+variable "subnet_id" {
+    default = "subnet-02acf2c93130b4eef"
+}
+
 variable "key_name" {
   description = "SSH key pair name"
   default     = "my-key-pair"
