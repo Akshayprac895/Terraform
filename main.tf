b5848657c5fe700f142267892087222f3e02f727
AkshayDevOps <134368829+Akshayprac895@users.noreply.github.com>
2025-07-25 13:38:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 
 
 module "module_ec2" {
-  source        = "terraform/module_ec2"  # path to your module_ec2
+  source        = "./terraform/module_ec2"  # path to your module_ec2
   ami_id        = "ami-05ffe3c48a9991133"
   instance_type = "t3.medium"
   subnet_id = "subnet-02acf2c93130b4eef"
