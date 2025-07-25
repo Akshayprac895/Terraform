43702d15e79e9880afb249722355c75e8d0b0eb3
AkshayDevOps <134368829+Akshayprac895@users.noreply.github.com>
2025-07-25 13:36:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-provider "aws" {
-    region = "us-east-1"
-}
+
 
 
 module "module_ec2" {
-  source        = "./terraform/module_ec2"  # path to your module_ec2
+  source        = "terraform/module_ec2"  # path to your module_ec2
   ami_id        = "ami-05ffe3c48a9991133"
   instance_type = "t3.medium"
   subnet_id = "subnet-02acf2c93130b4eef"
 }
 
 
-
+# provider "aws" {
+#    region = "us-east-1"
+# }
 
 #resource "aws_instance" "main" {
 #  ami =  var.ami_id
